75c3a346403bd196f7d49789a9352eb31547dcb8
Tahsin Shahriar <tahsin.ewucse@gmail.com>
2023-02-18 16:30:11 +0600

    form updated with validation

--- src/App.js
+++ src/App.js
@@ -12,24 +12,74 @@ function App() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState({
-        value: "",
-        isTouched: false,
-    });
+    const [password, setPassword] = useState("");
     const [role, setRole] = useState("role");
+
+    // capturing value for firstname
+    const setFName = (event) => {
+        setFirstName(event.target.value);
+    };
+
+    console.log(firstName);
+
+    // capturing value for lastname
+    const setLName = (event) => {
+        setLastName(event.target.value);
+    };
+
+    console.log(lastName);
+
+    // capturing value for email
+    const setEmailAddr = (event) => {
+        setEmail(event.target.value);
+    };
+
+    console.log(email);
+
+    // capturing value for password
+    const setPass = (event) => {
+        setPassword(event.target.value);
+    };
+
+    console.log(password);
+
+    // capturing value for email
+    const setUserRole = (event) => {
+        setRole(event.target.value);
+    };
+
+    console.log(role);
 
     const getIsFormValid = () => {
         // Implement this function
-        return true;
+        if (firstName && email && password && role) {
+            if (validateEmail(email)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     };
 
     const clearForm = () => {
         // Implement this function
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+        setRole("");
     };
 
-    const handleSubmit = () => {
-        alert("Account created!");
-        clearForm();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password.length < 8) {
+            PasswordErrorMessage();
+        } else {
+            alert("Account created!");
+            clearForm();
+        }
     };
 
     return (
@@ -41,29 +91,46 @@ function App() {
                         <label>
                             First name <sup>*</sup>
                         </label>
-                        <input placeholder="First name" />
+                        <input
+                            placeholder="First name"
+                            value={firstName}
+                            onChange={setFName}
+                        />
                     </div>
                     <div className="Field">
                         <label>Last name</label>
-                        <input placeholder="Last name" />
+                        <input
+                            placeholder="Last name"
+                            value={lastName}
+                            onChange={setLName}
+                        />
                     </div>
                     <div className="Field">
                         <label>
                             Email address <sup>*</sup>
                         </label>
-                        <input placeholder="Email address" />
+                        <input
+                            placeholder="Email address"
+                            value={email}
+                            onChange={setEmailAddr}
+                        />
                     </div>
                     <div className="Field">
                         <label>
                             Password <sup>*</sup>
                         </label>
-                        <input placeholder="Password" />
+                        <input
+                            placeholder="Password"
+                            value={password.value}
+                            onChange={setPass}
+                            type="password"
+                        />
                     </div>
                     <div className="Field">
                         <label>
                             Role <sup>*</sup>
                         </label>
-                        <select>
+                        <select value={role} onChange={setUserRole}>
                             <option value="role">Role</option>
                             <option value="individual">Individual</option>
                             <option value="business">Business</option>
